97231019bafa0ba1c89a7ecc68c13674d8b2ab1f
duzitong <duzitong811@126.com>
2018-09-16 03:16:12 -0700

    add script to build and run

--- main.c
+++ main.c
@@ -22,11 +22,16 @@ void run() {
     element_t sig;
     element_t temp1, temp2;
     element_t r1;
+    gmp_randstate_t state;
+    mpz_t a, q;
      
     char param[1024];
     size_t count = fread(param, 1, 1024, stdin);
     if (!count) pbc_die("input error");
     pairing_init_set_buf(pairing, param, count);
+
+    read_q(q, param);
+    gmp_printf("q: %Zd\n", q);
 
     element_init_G1(g, pairing);
     element_init_G2(h, pairing);
@@ -42,16 +47,14 @@ void run() {
     element_printf("g: %B\n", g);
     element_printf("h: %B\n", h);
     element_printf("r1: %B\n", r1);
-    printf("Finished.");
+    rnd_init(state);
+    rnd_q(a, state, q);
+    gmp_printf("Random q: %Zd\n", a);
+    printf("Finished.\n");
 }
 
 int main() {
-    gmp_randstate_t state;
-    mpz_t a, q;
-    mpz_set_ui(q, 5);
-    gmp_randinit_default(state);
-    mpz_urandomm(a, state, q);
-    gmp_printf("Random q: %Zd\n", a);
+    run();
     return 0;
 }
 
