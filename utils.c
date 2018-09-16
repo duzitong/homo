5febf965e78d4777cb144f5377d2bae529ed31a2
duzitong <duzitong811@126.com>
2018-09-16 18:07:03 +0800

    add init in utils

--- utils.c
+++ utils.c
@@ -3,12 +3,23 @@
 void read_q(mpz_t q, char* param) {
     char type[20], name[3][3], v[3][200];
     sscanf(param, "type %s\n%s %s\n%s %s\n%s %s", type, name[0], v[0], name[1], v[1], name[2], v[2]);
+    mpz_init(q);
     for (int i = 0; i < 3; i++) {
         if (strcmp(name[i], "n") == 0) {
             mpz_set_str(q, v[i], 10);
             return;
         }
     }
+}
+
+void rnd_init(gmp_randstate_t state) {
+    gmp_randinit_default(state);
+    gmp_randseed_ui(state, time(NULL));
+}
+
+void rnd_q(mpz_t r, gmp_randstate_t state, const mpz_t q) {
+    mpz_init(r);
+    mpz_urandomm(r, state, q);
 }
 
 void element_exp(element_t res, element_t a, int exp) {
--- utils.h
+++ utils.h
@@ -6,4 +6,7 @@
 
 void read_q(mpz_t, char*);
 
+void rnd_init(gmp_randstate_t);
+void rnd_q(mpz_t, gmp_randstate_t, const mpz_t);
+
 void element_exp(element_t, element_t, int);
